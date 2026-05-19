543dbe61d8346c691b19b99be7f661fd6e1d6f32
SIRI-Git31 <gaganaporeddy@gmail.com>
2026-05-19 12:24:08 +0000

    Added

---
+++ GMS/src/com/gms/model/Printer.java
@@ -0,0 +1,15 @@
+package com.gms.model;
+
+public class Printer {
+	int printerId;
+	String printerName;
+	String printerType;
+	//parameterized constructor
+	public Printer(int printerId, String printerName, String printerType) {
+		super();
+		this.printerId = printerId;
+		this.printerName = printerName;
+		this.printerType = printerType;
+	}
+
+}
