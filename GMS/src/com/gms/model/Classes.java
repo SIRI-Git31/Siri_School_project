0d0d1d9d1f1b53295c4a153c8ed1247fb88c766c
SIRI-Git31 <gaganaporeddy@gmail.com>
2026-05-19 12:19:42 +0000

    Completed

--- GMS/src/com/gms/model/Classes.java
+++ GMS/src/com/gms/model/Classes.java
@@ -3,5 +3,13 @@ package com.gms.model;
 public class Classes {
 int classId;
 String className;
+float classFee;
+//parameterized constructor
+public Classes(int classId, String className, float classFee) {
+	super();
+	this.classId = classId;
+	this.className = className;
+	this.classFee = classFee;
+}
 
 }
