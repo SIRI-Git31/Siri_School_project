680a7146b705849eba1de4c00f162f4573921cf1
SIRI-Git31 <gaganaporeddy@gmail.com>
2026-05-21 04:44:47 +0000

    setter getter method added

--- GMS/src/com/gms/model/Classes.java
+++ GMS/src/com/gms/model/Classes.java
@@ -11,5 +11,24 @@ public Classes(int classId, String className, float classFee) {
 	this.className = className;
 	this.classFee = classFee;
 }
-
+//setters and getters
+public int getClassId() {
+	return classId;
 }
+public void setClassId(int classId) {
+	this.classId = classId;
+}
+public String getClassName() {
+	return className;
+}
+public void setClassName(String className) {
+	this.className = className;
+}
+public float getClassFee() {
+	return classFee;
+}
+public void setClassFee(float classFee) {
+	this.classFee = classFee;
+}
+}
+
